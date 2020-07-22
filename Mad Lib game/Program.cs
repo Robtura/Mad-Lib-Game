5e62191065a842d80ec134f431dde08b9f45ca3e
Robtura <panterinasxy@yahoo.com>
2020-07-22 17:30:28 +0300

    Update 1.0

--- Mad Lib game/Program.cs
+++ Mad Lib game/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Drawing;
 
-namespace Mad_Lib_game
+namespace Mad_Lib_Game
 {
     class Program
     {
         static void Main(string[] args)
         {
             string color, pluralNoun, celebrity;
+
+            Console.Write("-------------------------------\n");
+            Console.Write("----------Mad Lib Game---------\n");
+            Console.Write("-------------------------------\n");
+
+            Console.WriteLine("");
 
             Console.Write("Enter a color: ");
             color = Console.ReadLine();
@@ -18,11 +24,20 @@ namespace Mad_Lib_game
             Console.Write("Enter a celebrity: ");
             celebrity = Console.ReadLine();
 
+            Console.WriteLine("");
+
+            Console.Write("-------------------------------\n");
+
+            Console.WriteLine("");
+
             Console.WriteLine("Roses are " + color );
             Console.WriteLine(pluralNoun + " are blue");
             Console.WriteLine("I Love " + celebrity);
 
-            Console.ReadLine();
+            Console.WriteLine("");
+            Console.Write("---Press any key to close------\n");
+
+            Console.ReadKey();
 
         }
     }
